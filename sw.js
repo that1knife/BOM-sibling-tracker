79e54c8506bcc2213918a3cdda48e8de27c26e9c
thomasholladay <86795038+that1knife@users.noreply.github.com>
2026-01-29 10:34:36 -0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = "bom-tracker-v1";
+
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/script.js",
+  "/manifest.json"
+];
+
+// Install
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache =>
+      cache.addAll(ASSETS)
+    )
+  );
+  self.skipWaiting();
+});
+
+// Activate
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(k => k !== CACHE_NAME)
+          .map(k => caches.delete(k))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// Fetch
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(res =>
+      res || fetch(event.request)
+    )
+  );
+});
