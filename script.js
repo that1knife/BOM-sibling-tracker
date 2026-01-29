330f003980c23480a472fbb16a31545b8e05706a
thomasholladay <86795038+that1knife@users.noreply.github.com>
2026-01-29 11:00:05 -0500

    Update script.js

--- script.js
+++ script.js
@@ -79,7 +79,7 @@ if (isStandalone) {
 
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
-    navigator.serviceWorker.register("/sw.js");
+    navigator.serviceWorker.register("/BOM-sibling-tracker/sw.js");
   });
 }
 
